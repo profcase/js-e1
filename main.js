94c78bc98e3e8ef6aa0ad3b3975676e5d37794e1
Denise Case <profcase@users.noreply.github.com>
2019-07-23 14:01:33 -0500

    updated bad link url

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const  calcArea = (length, width) => {
   return length * width
 }
 
-const hacked = "<h1><a href=http://shorturl.at/kDLO0>Lilly</a></h1>"
+const hacked = "<h1><a href="https://upload.wikimedia.org/wikipedia/commons/2/26/You_Have_Been_Hacked%21.jpg">Lilly</a></h1>"
 
 document.querySelector('#good').addEventListener('click',  () => {
   // Never trust the user. ALWAYS white list input text
